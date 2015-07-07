9a6de8dd465aa5ba51bc69414d03d140cf6c955f
AnnaPudlo <a.fomenko@gmail.com>
2015-07-07 14:48:56 +0300

    Refactoring, AddNumber and Restart are functions now Add function Delete

--- template.py
+++ template.py
@@ -21,6 +21,70 @@ bestScore = 0
 myFont = pygame.font.SysFont("Monospace", 30)
 colorWhite = (255, 255, 255)
 
+def AddNumber(x):
+    global currentNumber
+    global moveCount
+    i = int(x / cellSize)
+    j = size - 1
+    while gameTable[i][j] != 0 and j >= 0:
+        j -= 1
+    if gameTable[i][j] == 0:
+        gameTable[i][j] = currentNumber
+        currentNumber = random.randint(1, size)
+        moveCount -= 1
+
+
+def Delete():
+
+    global score
+
+    rowElCount = [0 for i in range(size)]
+    colElCount = [0 for i in range(size)]
+    delTable = []
+    for i in range(size):
+        delTable.append([0] * size)
+
+    for i in range(size):
+        rowElCount[i] = 0
+        colElCount[i] = 0
+        for j in range(size):
+            if gameTable[i][j] != 0:
+                rowElCount[i] += 1
+            if gameTable[j][i] != 0:
+                colElCount[i] += 1
+
+    for i in range(size):
+        for j in range(size):
+            if gameTable[i][j] != 0 and (gameTable[i][j] == rowElCount[i] or gameTable[i][j] == colElCount[j]):
+                delTable[i][j] = 1
+
+    for i in range(size):
+        for j in range(size):
+            if delTable[i][j] != 0:
+                gameTable[i][j] = 0
+                score += 1
+
+    for j in range(size):
+        i = size - 2
+        while i >= 0:
+            if gameTable[j][i] != 0 and gameTable[j][i + 1] == 0:
+                gameTable[j][i + 1] = gameTable[j][i]
+                gameTable[j][i] = 0
+            i -= 1
+
+
+def Restart():
+    global score
+    global bestScore
+    global moveCount
+    for i in range(size):
+        for j in range(size):
+            gameTable[i][j] = 0
+    if score > bestScore:
+        bestScore = score
+    score = 0
+    moveCount = 10
+
 
 def update():
     return
@@ -32,8 +96,9 @@ def draw():
         for j in range(size):
             pygame.draw.rect(screen, colorWhite, (i * cellSize, j * cellSize + 3 * cellSize, cellSize, cellSize),
                              2)
-            text = myFont.render(str(gameTable[i][j]), 1, colorWhite)
-            screen.blit(text, (i * cellSize, j * cellSize + 3 * cellSize, cellSize, cellSize))
+            if gameTable[i][j] != 0:
+                text = myFont.render(str(gameTable[i][j]), 1, colorWhite)
+                screen.blit(text, (i * cellSize, j * cellSize + 3 * cellSize, cellSize, cellSize))
 
     labelMove = myFont.render("Move", 1, colorWhite)
     screen.blit(labelMove, (0, 0, screenSize[0] / 3, cellSize))
@@ -41,15 +106,18 @@ def draw():
 
     labelScore = myFont.render("Score", 1, colorWhite)
     screen.blit(labelScore, (screenSize[0] / 3, 0, screenSize[0] / 3, cellSize))
-    screen.blit(myFont.render(str(score), 1, colorWhite), (screenSize[0] / 3, cellSize, screenSize[0] / 3, cellSize * 2))
+    screen.blit(myFont.render(str(score), 1, colorWhite),
+                (screenSize[0] / 3, cellSize, screenSize[0] / 3, cellSize * 2))
 
     labelRecord = myFont.render("Record", 1, colorWhite)
     screen.blit(labelRecord, (screenSize[0] * 2 / 3, 0, screenSize[0] / 3, cellSize))
-    screen.blit(myFont.render(str(bestScore), 1, (255, 255, 255)), (screenSize[0] * 2 / 3, cellSize, screenSize[0] / 3, cellSize * 2))
+    screen.blit(myFont.render(str(bestScore), 1, colorWhite),
+                (screenSize[0] * 2 / 3, cellSize, screenSize[0] / 3, cellSize * 2))
 
     labelNext = myFont.render("Next", 1, colorWhite)
     screen.blit(labelNext, (0, cellSize * 2, screenSize[0] / 3, cellSize * 3))
-    screen.blit(myFont.render(str(currentNumber), 1, colorWhite), (screenSize[0] / 3, cellSize * 2, cellSize, cellSize * 3))
+    screen.blit(myFont.render(str(currentNumber), 1, colorWhite),
+                (screenSize[0] / 3, cellSize * 2, cellSize, cellSize * 3))
 
     labelRestart = myFont.render("Restart", 1, colorWhite)
     screen.blit(labelRestart, (screenSize[0] / 3, cellSize * 10.5, screenSize[0] / 3, cellSize * 11.5))
@@ -70,22 +138,12 @@ while True:
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
                 if 10 * cellSize > event.pos[1] > 3 * cellSize:
-                    i = int(event.pos[0] / cellSize)
-                    j = size - 1
-                    while gameTable[i][j] != 0 and j >= 0:
-                        j -= 1
-                    if gameTable[i][j] == 0:
-                        gameTable[i][j] = currentNumber
-                        currentNumber = random.randint(1, size)
-                        moveCount -= 1
+                    AddNumber(event.pos[0])
                 if 2 * cellSize < event.pos[0] < 5 * cellSize and 10.4 * cellSize < event.pos[1] < 11.4 * cellSize:
-                    for i in range(size):
-                        for j in range(size):
-                            gameTable[i][j] = 0
-                    if score > bestScore:
-                        bestScore = score
-                    score = 0
-                    moveCount = 10
+                    Restart()
+    Delete()
+    if moveCount == 0:
+        moveCount = 10
 
     update()
     draw()
